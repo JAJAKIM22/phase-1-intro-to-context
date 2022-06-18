ef5cd8a87357184da412d28bc1cbc54f14f76a62
Peter Kimani <peterjajakim@gmail.com>
2022-06-18 12:09:30 +0300

    commit tree bc7bc6387cf3c4d5c49f10d194e8d2a049712cc8

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+let createEmployeeRecord = function(row){
+    return {
+        firstName: row[0],
+        familyName: row[1],
+        title: row[2],
+        payPerHour: row[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+let createEmployeeRecords = function(employeeRowData) {
+    return employeeRowData.map(function(row){
+        return createEmployeeRecord(row)
+    })
+}
+
+let createTimeInEvent = function(dateStamp){
+
+}
+
+let createTimeOutEvent = function(dateStamp){
+
+}
+
+let hoursWorkedOnDate = function(soughtDate){
+}
+
+let wagesEarnedOnDate = function(dateSought){
+}
+
+let allWagesFor = function () {
+}
+let findEmployeeByFirstName = function(srcArray, firstName) {
+}
+
+let calculatePayroll = function(arrayOfEmployeeRecords){
+}
